ab63293c255db1c6264ff916311fc5140b6fac81
mauraisa <aaron.maurais@bc.edu>
2019-01-20 22:37:34 -0500

    First commit.
    Working version of progrm.

---
+++ main.py
@@ -0,0 +1,58 @@
+
+import argparse
+import bibtexparser
+import os
+
+def splitNames(name_str):
+    """
+    Split author field into a list of "Name, Surname".
+
+    :param name_str: the record.
+    :type name_str: str
+    :returns: list -- list of author names
+
+    ..Note::More sensible version of bibtexparser Author function
+    which doesn't take record as a argument.
+
+    """
+
+    ret = bibtexparser.customization.getnames([i.strip() for i in name_str.replace('\n', ' ').split(" and ")])
+
+    return ret
+
+
+def fixKey(ent):
+    names = bibtexparser.customization.splitname(splitNames(ent['author'])[0])
+    return '{}{}'.format(names['last'][0], ent['year'])
+
+
+def main():
+    parser = argparse.ArgumentParser(description = 'Replace BibTex keys with author last name and year.')
+
+    parser.add_argument('-o', '--ofname', default='',
+                        help = 'Name of file to write. If this argument is left out, in_file is rewritten.')
+
+    parser.add_argument('in_file', help = 'Name of file to read.')
+
+    args = parser.parse_args()
+
+    print('Reading bibfile:\t\t  {}'.format(os.path.basename(args.in_file)))
+    with open(args.in_file, 'r') as bibtex_file:
+        bibDatabase = bibtexparser.load(bibtex_file)
+
+    for ent in bibDatabase.entries:
+        ent['ID'] = fixKey(ent)
+
+    if args.ofname == '':
+        ofname = args.if_file
+    else:
+        ofname = '{}/{}'.format(os.path.dirname(args.in_file), args.ofname)
+
+    print('Writing fixed bibfile to: {}'.format(os.path.basename(ofname)))
+    writer = bibtexparser.bwriter.BibTexWriter()
+    with open(ofname, 'w') as bibfile:
+        bibfile.write(writer.write(bibDatabase))
+
+
+if __name__ == '__main__':
+    main()
