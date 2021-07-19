90ad65178f7e0acb9c87eb51b2a55a6b368cfeff
mauraisa <aaron.maurais@bc.edu>
2021-07-19 19:11:46 -0400

    Add check and fix for duplicate keys.

--- bibtexKey/main.py
+++ bibtexKey/main.py
@@ -1,7 +1,9 @@
 
 import argparse
 import bibtexparser
+import sys
 import os
+from shutil import copy
 
 def splitNames(name_str):
     """
@@ -21,9 +23,28 @@ def splitNames(name_str):
     return ret
 
 
-def fixKey(ent):
-    names = bibtexparser.customization.splitname(splitNames(ent['author'])[0])
-    return '{}{}'.format(names['last'][0], ent['year'])
+def fixKey(ent, verbose=False):
+    try:
+        names = bibtexparser.customization.splitname(splitNames(ent['author'])[0])
+        key = '{}{}'.format(names['last'][0], ent['year'])
+    except KeyError:
+        if verbose:
+            sys.stderr.write('WARN: Could not find author field for entry!')
+            for k, v in ent.items():
+                sys.stderr.write('\n\t{}: {}'.format(k, v))
+            sys.stderr.write('\n')
+        key = ent['ID']
+    return key
+
+
+def refEqual(rhs, lhs):
+    try:
+        for key in rhs.keys():
+            if rhs[key] != lhs[key]:
+                return False
+    except KeyError:
+        return False
+    return True
 
 
 def main():
@@ -32,27 +53,55 @@ def main():
     parser.add_argument('-o', '--ofname', default='',
                         help = 'Name of file to write. If this argument is left out, in_file is rewritten.')
 
+    parser.add_argument('-v', '--verbose', choices=[0, 1], default=0, help='Verbose output?')
+
     parser.add_argument('in_file', help = 'Name of file to read.')
 
     args = parser.parse_args()
 
     print('Reading bibfile:\t\t  {}'.format(os.path.basename(args.in_file)))
+    copy(args.in_file, '{}.bak'.format(args.in_file))
     with open(args.in_file, 'r') as bibtex_file:
         bibDatabase = bibtexparser.load(bibtex_file)
 
+    new_bib = dict()
     for ent in bibDatabase.entries:
-        ent['ID'] = fixKey(ent)
+        key = fixKey(ent, args.verbose)
+        ent['ID'] = key
+        if key in new_bib:
+            if refEqual(ent, new_bib[key]):
+                if args.verbose:
+                    sys.stderr.write('WARN: found duplicate entries!')
+                    for k, v in ent.items():
+                        sys.stderr.write('\t{}: {}, {}\n'.format(k, v, new_bib[key][k]))
+
+            else:
+                for i in range(ord('a'), ord('z')):
+                    new_key = '{}{}'.format(key, chr(i))
+                    if new_key not in new_bib:
+                        ent['ID'] = new_key
+                        key = new_key
+                        break
+                if ent['ID'] in new_bib:
+                    raise RuntimeError('Ran out of possible key postfixes:\n\t{}\n'.format(ent))
+        new_bib[key] = ent
 
     if args.ofname == '':
         ofname = args.in_file
     else:
         ofname = args.ofname
 
+    # make new bib db
+    db = bibtexparser.bibdatabase.BibDatabase()
+    db.entries = list(new_bib.values())
+
+    # write new db
     print('Writing fixed bibfile to: {}'.format(os.path.basename(ofname)))
     writer = bibtexparser.bwriter.BibTexWriter()
     with open(ofname, 'w') as bibfile:
-        bibfile.write(writer.write(bibDatabase))
+        bibfile.write(writer.write(db))
 
 
 if __name__ == '__main__':
     main()
+
